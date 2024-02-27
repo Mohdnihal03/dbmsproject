ba5f8ead528899c5049b2e3840dd67919d93148d
nihall <mohammednihal9986@gmail.com>
2024-02-27 17:54:44 +0530

    pending work

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import restaurant_view , booking_view
+from .views import restaurant_view , booking_view, index
 
 urlpatterns = [
     path('restaurant_view/', restaurant_view, name='restaurant_view'),
+    path('', index, name='index'),
     # Other URL patterns for the restaurant app
     path('booking_view/', booking_view, name='booking_view'),
+
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def restaurant_view(request):
-    # Your logic for the restaurant view goes here
-    #  return HttpResponse("This is the restaurant view")
       return render(request,'restaurant/index.html')
 
 def booking_view(request):
-
       return render(request,'restaurant/about.html')
+
+def index(request):
+      return render(request,'restaurant/index.html')
